cbac08ccd5e114a5d6066d5de96e1f3c98233a9a
Pierre-Adrien Vasseur <pierreadrien.vasseur@gmail.com>
2025-01-06 20:36:02 +0100

    wip: trying live plugin

---
+++ live-plugin/generate-video.groovy
@@ -0,0 +1,53 @@
+import com.intellij.notification.*
+import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.progress.PerformInBackgroundOption
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.fileEditor.ex.FileEditorManagerEx
+import com.intellij.openapi.vfs.VirtualFile
+import static liveplugin.PluginUtil.*
+
+if (isIdeStartup) return
+
+registerAction(
+        id = "Generate video",
+        keyStroke = "control shift G",
+        actionGroupId = "ProjectViewPopupMenu", { AnActionEvent event ->
+
+    def project = event.project
+    if (project == null) {
+        show("No project detected.")
+        return
+    }
+
+    VirtualFile file = null
+    if (event.inputEvent instanceof java.awt.event.MouseEvent) {
+        def dataContext = event.getDataContext()
+        file = CommonDataKeys.VIRTUAL_FILE.getData(dataContext)
+    } else if (event.inputEvent instanceof java.awt.event.KeyEvent) {
+        def editorManager = FileEditorManagerEx.getInstanceEx(project)
+        file = editorManager.currentFile
+    }
+
+    if (file == null) {
+        show("No file selected or open.")
+        return
+    }
+
+    def docManager = FileDocumentManager.instance
+    def document = docManager.getDocument(file)
+    if (document != null) docManager.saveDocument(document)
+
+    if (file.extension != "videoml") {
+        show("Not a videoml file: ${file.path}")
+        return
+    }
+
+    show("File: ${file.path}")
+
+    doInBackground("Generate...", false, PerformInBackgroundOption.ALWAYS_BACKGROUND, {
+        // TODO
+        def command = "echo Generating video for ${file.path}"
+    })
+})
+
+show("Loaded generate video plugin")
